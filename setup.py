bbf1e159aacb5495669dd6379897c43bcb82cdee
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2020-07-13 10:46:54 +0200

    changed folder name and upped version to 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yongshi-pyqtgameboard",
-    version="0.1.1",
+    version="0.1.2",
     author="Michael-Yongshi",
     author_email="4registration@outlook.com",
     description="A gameboard widget for pyqt gui's package",
