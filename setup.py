571c616273f6a9cf6fa34fc1f2aa38db382cd806
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2020-07-13 10:41:28 +0200

    upped to version 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yongshi-pyqtgameboard",
-    version="0.1.0",
+    version="0.1.1",
     author="Michael-Yongshi",
     author_email="4registration@outlook.com",
     description="A gameboard widget for pyqt gui's package",
